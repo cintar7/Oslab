4b2e312a02c647907f9cbdd62ecb75d3fc5c8421
cintar7 <adsioros@gmail.com>
2023-07-24 23:58:38 +0300

    1 chapter.

---
+++ OS1/C_kernigham/Chapter1/outputClone.c
@@ -0,0 +1,19 @@
+//
+// Created by antouan on 24/7/2023.
+//
+
+# include <stdio.h>
+
+
+main () {
+
+    int c;
+
+    c = getchar();
+    while (c != EOF ){
+
+        putchar(c);
+        c = getchar();
+    }
+
+}
